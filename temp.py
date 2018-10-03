11db7a2f9ff97bcd32d8f5519f919d3c1c0e50df
kyL_lee <leekwanyao710@gmail.com>
2018-10-03 11:48:33 +0800

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+year=0
+month=0
+day=0
+n=0
+count=0
+year=input("請輸入年份(ex:2018)：")
+month=input("請輸入月份(ex:9)：")
+day=input("請輸入日期(ex:26)：")
+#----------------------------------------------
+count+=(year-1)*365+(year-1)/4-(year-1)/100+(year-1)/400
+#----------------------------------------------
+for i in range(month-1):
+    {
+         11 : lambda:,
+         10 : lambda:,
+         9  : lambda:,
+         8  : lambda:,
+         7  : lambda:,
+         6  : lambda:,
+         5  : lambda:,
+         4  : lambda:,
+         3  : lambda:,
+         2  : lambda:,
+         1  : lambda:,
+    
+    }.get(i)()
+#----------------------------------------------
+count+=day
+    
