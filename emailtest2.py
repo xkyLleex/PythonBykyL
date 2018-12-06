831327e05e69153e9f0db41ffc40a0d1d95a6f13
kyL_lee <leekwanyao710@gmail.com>
2018-12-07 00:26:45 +0800

    Delete emailtest2.py

--- emailtest2.py
+++
@@ -1,8 +0,0 @@
-import smtplib
-smtpserver = smtplib.SMTP_SSL("smtp.gmail.com",465)
-smtpserver.ehlo()
-smtpserver.login("ncutemail123@gmail.com","Email123")
-smtpserver.sendmail("ncutemail123@gmail.com","leekwanyao710@gmail.com",
-"Subject:")
-smtpserver.quit()
-print("send email success")
